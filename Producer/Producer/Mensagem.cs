0c1ac8ba7bc8e15bb2d5cb515ecbe0e073007752
Isabelle Ricciolini <87038843+isaricciolini@users.noreply.github.com>
2023-03-16 13:48:47 -0300

    Ajustes Producer

--- Producer/Producer/Mensagem.cs
+++ Producer/Producer/Mensagem.cs
@@ -9,8 +9,6 @@ namespace Producer
     public class Mensagem
     {
         public DateTime DataHora = DateTime.Now;
-        public int NumeroPDV { get; set; }
-        public string Operador { get; set; }
         public string Descricao { get; set; }
     }
 
--- Producer/Producer/Program.cs
+++ Producer/Producer/Program.cs
@@ -5,6 +5,7 @@ using Serilog.Sinks.SystemConsole.Themes;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Threading.Channels;
 
 namespace Producer
 {
@@ -18,31 +19,23 @@ namespace Producer
             logger.Information(
                 "Testando o envio de mensagens para uma Fila do RabbitMQ");
 
-
-            string queueName = "FilaIsa";
-            string json;
-
-            logger.Information($"Queue = {queueName}");
-
             try
             {
-                var factory = new ConnectionFactory()
+                var factory = new ConnectionFactory
                 {
-                    Uri = new Uri("amqps://bnyfjfyo:jjgYCL6Oxw2ZSQ6I5ajpuOVkKI-65knk@jackal.rmq.cloudamqp.com/bnyfjfyo"),
+                   HostName = "localhost",
                 };
 
                 using var connection = factory.CreateConnection();
                 using var channel = connection.CreateModel();
 
-                channel.QueueDeclare(queue: queueName,
+                channel.QueueDeclare(queue: "queue",
                                         durable: false,
                                         exclusive: false,
                                         autoDelete: false,
                                         arguments: null);
 
                 var mensagem = new Mensagem();
-                mensagem.NumeroPDV = 1;
-                mensagem.Operador = "Isabelle";
 
                     logger.Information(
                      "Escolha uma opção abaixo: \n " +
@@ -58,11 +51,10 @@ namespace Producer
                      "10- Acabando Numeração \n " +
                      "11- Modo Contingência \n " +
                      "12- Fechamento de Caixa \n" +
-                     ". - Enviar \n" +
                      "0- Sair");
 
                     var menu = Console.ReadLine();
-
+                
                     switch (menu)
                     {
                         case "0":
@@ -70,49 +62,51 @@ namespace Producer
                             break;
                         case "1":
                             mensagem.Descricao = "Abrir Gaveta de Valores";
-                            break;
+                            EnviarMensagem(mensagem, channel);
+                        break;
                         case "2":
                             mensagem.Descricao = "Abrir Caixa";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "3":
                             mensagem.Descricao = "Aplicar Acréscimo Item";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "4":
                             mensagem.Descricao = "Aplicar Acréscimo no Total";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "5":
                             mensagem.Descricao = "Aplicar Desconto no Item";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "6":
                             mensagem.Descricao = "Aplicar Desconto no Total";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "7":
                             mensagem.Descricao = "Cancelar Item";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "8":
                             mensagem.Descricao = "Cancelamento de Venda";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "9":
                             mensagem.Descricao = "Consultar Saldo Crediário";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "10":
                             mensagem.Descricao = "Numeração Acabando";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "11":
                             mensagem.Descricao = "Modo Contingência";
+                            EnviarMensagem(mensagem, channel);
                             break;
                         case "12":
                             mensagem.Descricao = "Fechar Caixa";
-                            break;
-                        case ".":
-                            json = JsonConvert.SerializeObject(mensagem);
-
-                            channel.BasicPublish(exchange: string.Empty,
-                            routingKey: "routingkey",
-                            basicProperties: null,
-                            body: Encoding.UTF8.GetBytes(json));
-
-                            Console.WriteLine("[x] Enviado {0}", mensagem);
+                            EnviarMensagem(mensagem, channel);
                             break;
                         default:
                             Console.WriteLine("Opção inválida!");
@@ -124,6 +118,19 @@ namespace Producer
                 logger.Error($"Exceção: {ex.GetType().FullName} | " +
                              $"Mensagem: {ex.Message}");
             }
+        }
+
+        private static void EnviarMensagem(Mensagem mensagem, IModel channel)
+        {
+            var json = JsonConvert.SerializeObject(mensagem);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: string.Empty,
+            routingKey: "queue",
+            basicProperties: null,
+            body: body);
+
+            Console.WriteLine($"Mensagem enviada: {mensagem.Descricao}");
         }
     }
 }
